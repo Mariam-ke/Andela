3cd41b934913a29f18ed6b2ebc8bfdbb4ad0a1da
Mariam-ke <39956740+Mariam-ke@users.noreply.github.com>
2018-10-12 13:43:18 +0300

    Update eight_ball.py

--- pre_interview/eight_ball.py
+++ pre_interview/eight_ball.py
@@ -4,6 +4,7 @@ import random
 
 class MagicBall():
 
+    # these are the answers that will display after questions load.
     answers = [
                'May be','That is a bad idea','Thats a great idea',
                'It is possible', 'I would advise so', 'No, No, No!', 'It is an unexpected move',
@@ -11,6 +12,7 @@ class MagicBall():
                'This is a random answer', 'The best it can be', 'Horrifingly doubtful'
               ]
 
+    #Request for user to input question with a 3 secs waiting time to load answer from the string above
     def question(self):
         print('Ask me a question.')
         input('>')
@@ -20,6 +22,10 @@ class MagicBall():
         self.replay()
 
 
+    #Ask another question, if answer is yes reload the above function to ask another question. If answer is no exit program
+    #If answer is neither yes or a no output 'I apologies, I did not catch that. Please repeat' 
+    #after 3 failed attempts output 'I apologies, I did not catch that. Please repeat' and exit game.
+    
     def replay(self):
         user_reply, play_attempts = "yes", 0
         while user_reply != "no":
@@ -38,13 +44,14 @@ class MagicBall():
                     print("I apologies, I did not catch that. Please repeat. \n")
                 play_attempts += 1
 
+    #Since it's case sensetive, if answer entered in uppercase change to lowercase
     def mock_replay_inputs(self):
         user_reply = str(input(">")).lower()
         print("input text '{}' has been detected.".format(user_reply))
         return user_reply
 
 
-
+#The below is to initialize with the test file
 if __name__ == '__main__':
     mgb = MagicBall()
     mgb.question()
